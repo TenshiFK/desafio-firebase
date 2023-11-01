8701d2ad335b3af34132cbb74bce8901a2c18311
TenshiFK <124812869+TenshiFK@users.noreply.github.com>
2023-10-31 22:23:57 -0300

    Update teste_tamanho.js

--- test/teste_tamanho.js
+++ test/teste_tamanho.js
@@ -6,12 +6,12 @@ const tamanhoMusica = require('../tamanho');
 describe('Tamanho dos textos', () => {
     it('Nome do cantor deve ser menor que a opinião', ()=>{
         const resultado = tamanhoCantor('A opinião sobre a música é essa!', 'Cantor1');
-        expect(resultado).to.equal(op > cantor);
+        expect(resultado).to.equal(op.length > cantor.length);
     });
 
     it('Nome da música deve ser Menor que a opinião', ()=>{
         const resultado = tamanhoMusica('A opinião sobre a música é essa!', 'Musica1');
-        expect(resultado).to.equal(op > musica);
+        expect(resultado).to.equal(op.length > musica.length);
     });
 
 })
